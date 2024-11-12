1754f220d1b28669574c4d8ac39f3ad5fa38ea5d
vinzenzm <vinzenz@malke.info>
2024-11-12 12:45:37 +0100

    fixed build to work with new interfaces

--- build.sh
+++ build.sh
@@ -7,12 +7,19 @@ ROOT=$PWD
 cd src/dependencies/crazyflie_hardware/src/crazyflie_interfaces
 touch COLCON_IGNORE
 CRAZYFLIE_INTERFACES=$PWD
-
 cd $ROOT
+
+cd src/dependencies/crazyflie_hardware/src/crazyflie_interfaces_python
+touch COLCON_IGNORE
+CRAZYFLIE_INTERFACES_PYTHON=$PWD
+cd $ROOT
+
 colcon build
 
 
 cd $CRAZYFLIE_INTERFACES
+rm COLCON_IGNORE
+cd $CRAZYFLIE_INTERFACES_PYTHON
 rm COLCON_IGNORE
 
 cd $ROOT
